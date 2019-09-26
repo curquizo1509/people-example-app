0a59179191ad947154dd98fbf3467f6c9b4603bf
Camila Micaela Urquizo Suca <curquizo@belatrix.com>
2019-09-26 18:40:07 -0500

    Resuelto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'examples',
+    loadChildren: () => import('./modules/examples/examples.module').then( m => m.ExamplesModule)
+  },
+  {
+    path: 'articles',
+    loadChildren: () => import('./modules/articles/articles.module').then( m => m.ArticlesModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
